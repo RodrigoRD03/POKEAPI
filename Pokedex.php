d73fe5217378f49bedf7684c6430f1f8417defbe
ROdrigoRRRRD <Roddrigo2003cod@gmail.com>
2023-01-05 23:21:48 -0600

    navbar full

--- Pokedex.php
+++ Pokedex.php
@@ -4,42 +4,87 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Styles.css">
+    <link rel="stylesheet" href="Style.css">
     <link rel="icon" href="./images/logo.ico">
+    <script src="https://kit.fontawesome.com/4b5992b75f.js" crossorigin="anonymous"></script>
     <title>Pokedex</title>
 </head>
-<body>
+<body class="pokedex-GridContainer">
+    <nav class="navbar-Container">
+        <div class="logo-Container">
+            <a href="./index.php">
+                <img  class="logo-Image" src="./images/Pokemon.png" alt="">
+            </a>
+        </div>
+        <div class="form-SendPokemon">
+            <form action="./Pokedex.php" method="post">
+                <input class="input-Pokemon" required type="text" name="findPokemon" placeholder="Search A Pokemon">
+                <button class="button-FindPokemon" type="submit"><i class="fa-sharp fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+        <div class="list-Options">
+            <div class="screenPhone">
+                <div class="image-FloatContainer">
+                    <img class="navbar-FloatImage" src="./images/Pokeball.png" alt="">
+                </div>
+                <div class="navbar-FloatMenu">
+                    <ul>
+                        <p>Pages</p> 
+                        <hr>
+                        <li class="pink"><a class="option-Home pink" href="./index.php">Home</a></li>
+                        <hr>
+                        <li class="yellow"><a class="option-Home yellow" href="./Pokedex.php">Pokedex</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="screenComputer">
+                <div class="links-Content">
+                    <div class="linkHome">
+                        <a href="./index.php">Home</a>
+                    </div>
+                    <div class="linkPokedex">
+                        <a href="./Pokedex.php">Pokedex</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
     <?php
-        $nombre = $_POST['opciones'];
-        echo $nombre."<br>";
+        if(isset($_POST['findPokemon']) == false){
+            $numeber_Random = random_int(1, 100);
+            $name = "$numeber_Random";
+        } else {
+            $name = $_POST['findPokemon'];
+            $name = strtolower($name);
+        }
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/pokemon/ditt");
+        curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/pokemon/".$name);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         $data = curl_exec($ch);
-        curl_close($ch);
-        $pokemon = json_decode($data);
-        $dato = "official-artwork";
-        $imagen = $pokemon->sprites->other->dream_world->front_default;
-        if($imagen == null){
-            $imagen = $pokemon->sprites->other->$dato->front_default;
-        }
-        $type = $pokemon->types;
-        $names = $pokemon->forms;
-        
-        foreach($type as $types){
-            echo "<p>".$types->type->name."</p>";
-        }
-    ?>
-    <section class="container-Card">
-        <div class="random-color">
-            <?php
-                echo "<img class='card-image' src='$imagen' alt=''>";
-            ?>
+            curl_close($ch);
+            $pokemon = json_decode($data);
+        if($data == "Not Found"){
+            echo "<section class='pokedex-section'>
+                    <h1><span><i class='fa-solid fa-magnifying-glass'></i></span>&nbspSearch Not Found</h1>
+                </section>";
+        } else {
+            $dato = "official-artwork";
+            $imagen = $pokemon->sprites->other->dream_world->front_default;
+            if($imagen == null){
+                $imagen = $pokemon->sprites->other->$dato->front_default;
+            }
+            $type = $pokemon->types;
+            $names = $pokemon->forms;
             
-        </div>
-        <div class="random-color-2">
-            <?php
+            foreach($type as $types){
+                echo "<p>".$types->type->name."</p>";
+            }
+             echo "<section class='container-Card' styles='margin-top: 100px; color=white;'>
+            <div class='random-color'>";
+                echo "<img class='card-image' src='$imagen' alt=''>
+            </div>
+            <div class='random-color-2'>";
                 echo "<h1 class='pokemon-name'>$pokemon->name</h1>";
                 echo "<h3>Tipo</h3>";
                 $type = $pokemon->types;
@@ -49,9 +94,10 @@
                 $ability = $pokemon->abilities;
                 foreach($ability as $abilities){
                     echo "<p>".$abilities->ability->name."</p>";
-                }
-            ?>
-        </div>
-    </section>
+                }   
+            echo "</div>
+            </section>";
+        }
+    ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,32 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Styles.css">
+    <link rel="stylesheet" href="./Style.css">
     <link rel="icon" href="./images/logo.ico">
-    <title>Document</title>
+    <script src="https://kit.fontawesome.com/4b5992b75f.js" crossorigin="anonymous"></script>
+    <title>Home</title>
 </head>
     <?php
+use function PHPSTORM_META\type;
         function backgrounds($types){
-            $type = $types[0]->type->name;
-            $space;
-            $colorOne;
-            $colorTwo;
-            $TypeC = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon","dark", "fairy"];
-            $TypeC1 = ["hsla(187, 100%, 89%, 1)", "hsla(191, 100%, 96%, 1)", "hsla(133, 94%, 49%, 1)", "hsla(31, 12%, 59%, 1)", "hsla(31, 24%, 39%, 1)", "hsla(114, 63%, 61%, 1)", "hsla(0, 0%, 89%, 1)", "hsla(0, 0%, 100%, 1)", "hsla(44, 74%, 55%, 1)", "hsla(164, 67%, 68%, 1)", "hsla(59, 86%, 68%, 1)", "hsla(0, 0%, 99%, 1)", "hsla(0, 0%, 100%, 1)", "hsla(209, 88%, 76%, 1)", "hsla(19, 87%, 59%, 1)", "hsla(216, 84%, 47%, 1)", "hsla(303, 73%, 85%, 1)", "hsla(19, 100%, 58%, 1)"];
-            $TypeC2 = ["hsla(239, 100%, 67%, 1)", "hsla(191, 89%, 75%, 1)", "hsla(164, 97%, 15%, 1)", "hsla(32, 44%, 24%, 1)", "hsla(31, 75%, 11%, 1)", "hsla(30, 15%, 33%, 1)", "hsla(0, 0%, 16%, 1)", "hsla(0, 0%, 41%, 1)", "hsla(0, 100%, 58%, 1)", "hsla(182, 32%, 36%, 1)", "hsla(134, 36%, 53%, 1)", "hsla(44, 100%, 50%, 1)", "hsla(269, 65%, 55%, 1)", "hsla(196, 66%, 32%, 1)", "hsla(28, 59%, 26%, 1)", "hsla(0, 0%, 0%, 1)", "hsla(303, 68%, 43%, 1)", "hsla(0, 0%, 25%, 1)"];
+            $type = $types[0]->type->name;$TypeC = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon","dark", "fairy"];
+            $TypeC1 = ["hsla(187, 100%, 89%, 1)", "hsla(195, 89%, 28%, 1)", "hsla(191, 100%, 96%, 1)", "hsla(133, 94%, 49%, 1)", "hsla(31, 12%, 59%, 1)", "hsla(31, 24%, 39%, 1)", "hsla(114, 63%, 61%, 1)", "hsla(0, 0%, 89%, 1)", "hsla(0, 0%, 100%, 1)", "hsla(44, 74%, 55%, 1)", "hsla(164, 67%, 68%, 1)", "hsla(59, 86%, 68%, 1)", "hsla(0, 0%, 99%, 1)", "hsla(0, 0%, 100%, 1)", "hsla(209, 88%, 76%, 1)", "hsla(19, 87%, 59%, 1)", "hsla(216, 84%, 47%, 1)", "hsla(303, 73%, 85%, 1)", "hsla(19, 100%, 58%, 1)"];
+            $TypeC2 = ["hsla(239, 100%, 67%, 1)", "hsla(1, 79%, 48%, 1)", "hsla(191, 89%, 75%, 1)", "hsla(164, 97%, 15%, 1)", "hsla(32, 44%, 24%, 1)", "hsla(31, 75%, 11%, 1)", "hsla(30, 15%, 33%, 1)", "hsla(0, 0%, 16%, 1)", "hsla(0, 0%, 41%, 1)", "hsla(0, 100%, 58%, 1)", "hsla(182, 32%, 36%, 1)", "hsla(134, 36%, 53%, 1)", "hsla(44, 100%, 50%, 1)", "hsla(269, 65%, 55%, 1)", "hsla(196, 66%, 32%, 1)", "hsla(28, 59%, 26%, 1)", "hsla(0, 0%, 0%, 1)", "hsla(303, 68%, 43%, 1)", "hsla(0, 0%, 25%, 1)"];
             for($i = 0; $i < 18; $i++){
                 if($type == $TypeC[$i]){
                     $space = $i;
                 }
             }
-            $colorOne = $TypeC1[$space - 1];
-            $colorTwo = $TypeC2[$space - 1];
+            $colorOne = $TypeC1[$space];
+            $colorTwo = $TypeC2[$space];
             echo "<div class='image-Container' style='background: linear-gradient(45deg, $colorOne 0%, $colorTwo 100%)'>";
         }
         function prints($types){
             $TypeC = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon","dark", "fairy"];
-            $TypeC1 = ["hsla(187, 100%, 89%, 1)", "hsla(191, 100%, 96%, 1)", "hsla(133, 94%, 49%, 1)", "hsla(31, 12%, 59%, 1)", "hsla(31, 24%, 39%, 1)", "hsla(114, 63%, 61%, 1)", "hsla(0, 0%, 89%, 1)", "hsla(0, 0%, 100%, 1)", "hsla(44, 74%, 55%, 1)", "hsla(164, 67%, 68%, 1)", "hsla(59, 86%, 68%, 1)", "hsla(0, 0%, 99%, 1)", "hsla(0, 0%, 100%, 1)", "hsla(209, 88%, 76%, 1)", "hsla(19, 87%, 59%, 1)", "hsla(216, 84%, 47%, 1)", "hsla(303, 73%, 85%, 1)", "hsla(19, 100%, 58%, 1)"];
-            $TypeC2 = ["hsla(239, 100%, 67%, 1)", "hsla(191, 89%, 75%, 1)", "hsla(164, 97%, 15%, 1)", "hsla(32, 44%, 24%, 1)", "hsla(31, 75%, 11%, 1)", "hsla(30, 15%, 33%, 1)", "hsla(0, 0%, 16%, 1)", "hsla(0, 0%, 41%, 1)", "hsla(0, 100%, 58%, 1)", "hsla(182, 32%, 36%, 1)", "hsla(134, 36%, 53%, 1)", "hsla(44, 100%, 50%, 1)", "hsla(269, 65%, 55%, 1)", "hsla(196, 66%, 32%, 1)", "hsla(28, 59%, 26%, 1)", "hsla(0, 0%, 0%, 1)", "hsla(303, 68%, 43%, 1)", "hsla(0, 0%, 25%, 1)"];
+            $TypeC1 = ["hsla(187, 100%, 89%, 1)", "hsla(195, 89%, 28%, 1)", "hsla(191, 100%, 96%, 1)", "hsla(133, 94%, 49%, 1)", "hsla(31, 12%, 59%, 1)", "hsla(31, 24%, 39%, 1)", "hsla(114, 63%, 61%, 1)", "hsla(0, 0%, 89%, 1)", "hsla(0, 0%, 100%, 1)", "hsla(44, 74%, 55%, 1)", "hsla(164, 67%, 68%, 1)", "hsla(59, 86%, 68%, 1)", "hsla(0, 0%, 99%, 1)", "hsla(0, 0%, 100%, 1)", "hsla(209, 88%, 76%, 1)", "hsla(19, 87%, 59%, 1)", "hsla(216, 84%, 47%, 1)", "hsla(303, 73%, 85%, 1)", "hsla(19, 100%, 58%, 1)"];
+            $TypeC2 = ["hsla(239, 100%, 67%, 1)", "hsla(1, 79%, 48%, 1)", "hsla(191, 89%, 75%, 1)", "hsla(164, 97%, 15%, 1)", "hsla(32, 44%, 24%, 1)", "hsla(31, 75%, 11%, 1)", "hsla(30, 15%, 33%, 1)", "hsla(0, 0%, 16%, 1)", "hsla(0, 0%, 41%, 1)", "hsla(0, 100%, 58%, 1)", "hsla(182, 32%, 36%, 1)", "hsla(134, 36%, 53%, 1)", "hsla(44, 100%, 50%, 1)", "hsla(269, 65%, 55%, 1)", "hsla(196, 66%, 32%, 1)", "hsla(28, 59%, 26%, 1)", "hsla(0, 0%, 0%, 1)", "hsla(303, 68%, 43%, 1)", "hsla(0, 0%, 25%, 1)"];
             $colorOne = "";
             $colorTwo = "";
             $space = 0;
@@ -37,8 +35,8 @@
                 for($i = 0; $i < 18; $i++){
                     if($type->type->name == $TypeC[$i]){
                         $space = $i;
-                        $colorOne = $TypeC1[$space - 1];
-                        $colorTwo = $TypeC2[$space - 1];
+                        $colorOne = $TypeC1[$space];
+                        $colorTwo = $TypeC2[$space];
                     }
                 }
                 echo "<div class='background-type' style='background: linear-gradient(45deg, $colorOne 0%, $colorTwo 100%)'>";
@@ -71,59 +69,110 @@
         }
     ?>
 <body class="grid-Container">
-    <nav class="navbar-Conteiner">
-        <div class="background">
-            <a href="./index.php"><h3 class="tittle-Background">Pokemon</h3></a>
+    <nav class="navbar-Container">
+        <div class="logo-Container">
+            <a href="./index.php">
+                <img  class="logo-Image" src="./images/Pokemon.png" alt="">
+            </a>
+        </div>
+        <div class="form-SendPokemon">
+            <form action="./Pokedex.php" method="post">
+                <input class="input-Pokemon" required type="text" name="findPokemon" placeholder="Search A Pokemon">
+                <button class="button-FindPokemon" type="submit"><i class="fa-sharp fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+        <div class="list-Options">
+            <div class="screenPhone">
+                <div class="image-FloatContainer">
+                    <img class="navbar-FloatImage" src="./images/Pokeball.png" alt="">
+                </div>
+                <div class="navbar-FloatMenu">
+                    <ul>
+                        <p>Pages</p> 
+                        <hr>
+                        <li class="pink"><a class="option-Home pink" href="./index.php">Home</a></li>
+                        <hr>
+                        <li class="yellow"><a class="option-Home yellow" href="./Pokedex.php">Pokedex</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="screenComputer">
+                <div class="links-Content">
+                    <div class="linkHome">
+                        <a href="./index.php">Home</a>
+                    </div>
+                    <div class="linkPokedex">
+                        <a href="./Pokedex.php">Pokedex</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </nav>
     <section class="Cards-PokeInitials">
+        <div class="background">
+            <h3 class="tittle-Background">Starter Pokemon</h3>
+        </div>
         <?php
-            $pokemon = ["bulbasaur", "charmander", "squirtle"];
-            foreach($pokemon as $pokemones){
-                $ch = curl_init();
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/pokemon/".$pokemones);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_HEADER, 0);
-                $data = curl_exec($ch);
-                curl_close($ch);
-                $character = json_decode($data);
-                $imagen = $character->sprites->other->dream_world->front_default;
-                if($imagen == null){
-                    $imagen = $character->sprites->other->$dato->front_default;
-                }
-                $types = $character->types;
-                $abilities = $character->abilities;
-                $stats = $character->stats;
-                echo "<div class='poke-card'>";
-                    backgrounds($types);
-                        echo "<img class='poke-image' src='$imagen' alt=''>";
-                    echo "</div>";
-                    echo "<div class='poke-info'>";
-                        $name = $character->name;
-                        echo "<div class='poke-name'>
-                            <h1 class='poke-Name'>".$name."</h1>
-                            </div>
-                            <div class='poke-Type'>
-                                <h3 class='types-tittle'>Types</h3>";
-                                prints($types);
-                            echo "</div>
-                            <div class='poke-Abilities'>
-                                <h3 class='abilities'>Abilities</h3>";
-                                abilities($abilities);
-                            echo "</div>";
-                            stats($stats);
-                            echo "<div class='form-conteiner'>
-                                <form action='./Pokedex.php' method='post'>
-                                    <input type='text' name='opciones' style='display: none' value='{$name}'>
-                                <button class='moreInfo-Button' type='submit'><h3>more</h3></button>
-                                </form>
-                            </div>";
-                    echo "</div>";
+        $pokemon = ["bulbasaur", "charmander", "squirtle"];
+        foreach($pokemon as $pokemones){
+            $ch = curl_init();
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "https://pokeapi.co/api/v2/pokemon/".$pokemones);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            $data = curl_exec($ch);
+            curl_close($ch);
+            $character = json_decode($data);
+            $imagen = $character->sprites->other->dream_world->front_default;
+            if($imagen == null){
+                $imagen = $character->sprites->other->$dato->front_default;
+            }
+            $types = $character->types;
+            $abilities = $character->abilities;
+            $stats = $character->stats;
+            echo "<div class='poke-card'>";
+                backgrounds($types);
+                    echo "<img class='poke-image' src='$imagen' alt=''>";
                 echo "</div>";
+                echo "<div class='poke-info'>";
+                    $name = $character->name;
+                    echo "<div class='poke-name'>
+                        <h1 class='poke-Name'>".$name."</h1>
+                        </div>
+                        <div class='poke-Type'>
+                            <h3 class='types-tittle'>Types</h3>";
+                            prints($types);
+                        echo "</div>
+                        <div class='poke-Abilities'>
+                            <h3 class='abilities'>Abilities</h3>";
+                            abilities($abilities);
+                        echo "</div>";
+                        stats($stats);
+                        echo "<div class='form-conteiner'>
+                            <form action='./Pokedex.php' method='post'>
+                                <input type='text' name='findPokemon' style='display: none' value='{$name}'>
+                            <button class='moreInfo-Button' type='submit'><i class='fa-solid fa-chevron-right'></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>";
             }
         ?>
     </section>
+    <aside class="aside-Content">
+        <h2>Types</h2>
+        <?php
+            $TypeC = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon","dark", "fairy"];
+            for($i = 0; $i < 18; $i++){ 
+                echo "<hr>
+                <form action='./Types.php' mothod='post'>
+                <input type='text' class='hidenInput' name='types' value={$TypeC[$i]}'> 
+                <button class='aside-Content-button' type='submit' ><h4>{$TypeC[$i]}</h4></button>
+                </form>
+                ";
+            }
+        ?>
+    </aside>
     <script src="./main.js"></script>
 </body>
 </html>
